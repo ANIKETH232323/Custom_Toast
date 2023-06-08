5ce4680ce24f6a6e818be9d44f383b79e4eaeb9f
Aniketh Pratihar <aniketh5660@gmail.com>
2023-06-08 21:06:39 +0530

    Done

--- app/src/main/java/com/example/custom_toast/MainActivity.java
+++ app/src/main/java/com/example/custom_toast/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 import com.example.custom_image_toast.Toast_Custom;
